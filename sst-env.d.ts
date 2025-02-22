3973add6d9be01e59fc3795992515d33dc445af0
Leo Allesch <leoallesch@gmail.com>
2025-02-21 20:50:56 -0500

    v1.1

--- sst-env.d.ts
+++ sst-env.d.ts
@@ -5,6 +5,10 @@
 
 declare module "sst" {
   export interface Resource {
+    "Frontend": {
+      "type": "sst.aws.Nextjs"
+      "url": string
+    }
   }
 }
 /// <reference path="sst-env.d.ts" />
