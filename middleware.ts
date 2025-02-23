39a948ea33bd5e7d50993134d72a1dc344fb0e3a
Ritesh Dhakal <37334512+rdhakal098@users.noreply.github.com>
2025-02-22 20:47:58 -0500

    push updates jobs and programs

--- middleware.ts
+++ middleware.ts
@@ -1,33 +1,33 @@
-import { betterFetch } from "@better-fetch/fetch";
-import { Session } from "better-auth";
-import { NextResponse, type NextRequest } from "next/server";
+import {betterFetch} from "@better-fetch/fetch";
+import {Session} from "better-auth";
+import {NextResponse,type NextRequest} from "next/server";
 
 export default async function authMiddleware(request: NextRequest) {
-  const isAuthPage =
+  const isAuthPage=
     request.nextUrl.pathname.startsWith("/auth");
-  const isAppPage = request.nextUrl.pathname.startsWith("/dashboard");
+  const isAppPage=request.nextUrl.pathname.startsWith("/dashboard");
 
-  const { data: session } = await betterFetch<Session>(
+  const {data: session}=await betterFetch<Session>(
     "/api/auth/get-session",
     {
       baseURL: request.nextUrl.origin,
       headers: {
-        cookie: request.headers.get("cookie") || "",
+        cookie: request.headers.get("cookie")||"",
       },
     },
   );
 
-  if (session && isAuthPage) {
-    return NextResponse.redirect(new URL("/dashboard", request.url));
+  if(session&&isAuthPage) {
+    return NextResponse.redirect(new URL("/dashboard",request.url));
   }
 
-  if (!session && isAppPage) {
-    return NextResponse.redirect(new URL("/auth/login", request.url));
+  if(!session&&isAppPage) {
+    return NextResponse.redirect(new URL("/auth/login",request.url));
   }
 
   return NextResponse.next();
 }
 
-export const config = {
-  matcher: ["/dashboard/:path*", "/auth/:path*"],
+export const config={
+  matcher: ["/dashboard/:path*","/auth/:path*"],
 };
