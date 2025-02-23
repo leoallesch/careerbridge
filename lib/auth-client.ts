15439515cfbaf83fd564adfc62458ac2fea7e9c8
leoallesch <leoallesch@gmail.com>
2025-02-22 21:35:53 -0500

    Update auth-client.ts

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,6 +1,6 @@
 import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
-    baseURL: "http://localhost:3000" // the base url of your auth server
+    baseURL: "https://careerbridge-gules.vercel.app" // the base url of your auth server
 })
 
 export const {
